e2b3ce747c539d207c48291140e696d50529005e
oricalvo <ori.calvo@gmail.com>
2016-11-10 01:16:48 +0200

    Upgrading to anglar 2.1.2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,7 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    moduleId: module.id,
-    templateUrl: './app.component.html',
-    styleUrls: ["./app.component.css"],
-    encapsulation: ViewEncapsulation.None,
+    template: require("./app.component.html"),
+    styles: [require("./app.component.css")]
 })
-export class AppComponent {
-    title: string;
-
-    constructor() {
-        this.title = "Angular2 Min Seed";
-    }
-}
+export class AppComponent { }
