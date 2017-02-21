7892535c63c5b64fa6135a49cab20dd174fb3643
oricalvo <ori.calvo@gmail.com>
2017-02-21 23:25:10 +0200

    Upgrade to Angular 2.4.8

--- build/dev.js
+++ build/dev.js
@@ -1,17 +1,17 @@
 const shelljs = require("shelljs");
 const path = require("path");
 
-const tscFilePath = path.join(__dirname, "../node_modules/.bin/tsc");
+const tscFilePath = "\"" + path.join(__dirname, "../node_modules/.bin/tsc") + "\"";
 if(shelljs.exec(tscFilePath).code != 0) {
     console.error("Typscript compilation failed");
     return;
 }
 
-const sass = path.join(__dirname, "../node_modules/.bin/node-sass.cmd --recursive ./app --output ./app");
+const sass = "\"" + path.join(__dirname, "../node_modules/.bin/node-sass.cmd") + "\"" + " --recursive ./app --output ./app";
 if(shelljs.exec(sass).code != 0) {
     console.error("SCSS compilation failed");
     return;
 }
 
-const sjs = path.join(__dirname, "../node_modules/.bin/sjs");
+const sjs = "\"" + path.join(__dirname, "../node_modules/.bin/sjs") + "\"";
 shelljs.exec(sjs);
