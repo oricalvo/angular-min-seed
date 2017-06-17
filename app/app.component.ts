adb355d9c022236908f11c94801530a41b86f1d9
oricalvo <ori.calvo@gmail.com>
2017-06-17 08:58:45 +0300

    More

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  contacts: Contact[];
+
   constructor() {
+    this.contacts = [
+      {id:1, name: "Ori"},
+      {id:2, name: "Roni"},
+    ];
   }
+}
+
+export interface Contact {
+    id: number;
+    name: string;
 }
